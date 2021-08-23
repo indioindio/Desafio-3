c2f3656575ff802d508f602990594091d52c2275
indioindio <89296886+indioindio@users.noreply.github.com>
2021-08-22 22:13:25 -0300

    Add files via upload

---
+++ database.js
@@ -0,0 +1,77 @@
+const sequence = {
+    _id: 1,
+    get id() { return this._id++}
+}
+
+const pokemons = []
+
+function salvarpokemons(pokemon) {
+   if(!pokemon.id) pokemon.id = sequence.id
+   pokemons[pokemon.id] = pokemon
+   return pokemon 
+}
+
+function mostrarpokemon(id) {
+    return pokemons[id] || {}
+}
+
+function mostrarpokemons() {
+    return Object.values(pokemons)
+
+}
+
+function atualizarpokemon(id, pokemon) {
+    pokemons[id] = pokemon
+    return pokemon
+
+}
+
+function deletarpokemon(id) {
+    sequence._id = sequence._id - 1
+    const pokemondeletado = pokemon[id]
+    pokemons.splice(id, 1)
+    pokemons.forEach(pokemon => {
+        if(pokemon.id > id)
+            pokemon.id = pokemon.id - 1
+    })
+    return pokemondeletado
+}
+
+function batalhapokemon(id1, id2) {
+    const superEfetivo = 40
+    const Efetivo = 20
+    const naoEfetivo = 10 
+
+    const pokemon1 = pokemons[id1]
+    const pokemon2 = pokemons[id2]
+
+    if(pokemon1.hp != 0 && pokemon2.hp != 0){
+        if(pokemon1.tipo == pokemon2.fraqueza) {
+            pokemon2.hp = pokemon2.hp - superEfetivo
+        } else if (pokemon1.tipo == pokemon2.resistencia){
+            pokemon2.hp = pokemon2.hp - naoEfetivo
+        } else {
+            pokemon2.hp = pokemon2.hp - Efetivo
+
+        }
+    }
+    if(pokemon1.hp != 0 && pokemon2.hp != 0){
+        if(pokemon2.tipo == pokemon1.fraqueza) {
+            pokemon1.hp = pokemon1.hp - superEfetivo
+        } else if (pokemon1.tipo == pokemon2.resistencia){
+            pokemon1.hp = pokemon1.hp - naoEfetivo
+        } else {
+            pokemon1.hp = pokemon1.hp - Efetivo
+
+        }
+    }
+    if(pokemon1.hp <0) pokemon1.hp =0
+    if(pokemon2.hp <0) pokemon1.hp =0
+
+    return `${pokemon1.nome}: ${pokemon1.hp} / ${pokemon2.nome}: ${pokemon2.hp}`
+
+}
+
+
+
+module.exports = {salvarpokemons, mostrarpokemon, mostrarpokemons, atualizarpokemon, deletarpokemon, batalhapokemon }
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const app = express()
+const dataBase = require ('./dataBase')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: true }))
+
+app.get ('/pokemons', (req, res) => {
+    res.send(dataBase.mostrarpokemons())})
+
+    app.get ('/pokemons/:id', (req, res) => {
+    res.send(dataBase.mostrarpokemons(req.params.id))})
+
+    app.post ('/pokemons', (req, res) => {
+    const pokemon = dataBase.salvarpokemons({
+        nome: req.body.nome,
+        tipo: req.body.tipo,
+        fraqueza: req.body.fraqueza,
+        resistencia: req.body.resistencia,
+        hp: 100,
+
+    })
+    res.send(pokemon)
+
+ })
+
+ app.put ('/pokemons/', (req, res) => {
+    const pokemon = database.atualizarpokemon(req.params.id, {
+        nome: req.body.nome,
+        tipo: req.body.tipo,
+        fraqueza: req.body.fraqueza,
+        resistencia: req.body.resistencia,
+        hp: 100,
+        id: parseInt(req.params.id)
+
+    })  
+
+
+    res.send(pokemon)
+
+})
+
+app.delete ('/pokemons/:id', (req, res) => {
+    res.send(database.deletarpokemon(req.params.id))
+    })  
+
+    app.post ('/batalha', (req, res) =>{
+        res.send(dataBase.batalhapokemon(req.body.id1, req.body.id2))
+    })
+
+
+app.listen(3003)
